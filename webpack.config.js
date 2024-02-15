34b08c43985052f19a7438f88566fe59eee59202
MDz1985 <m_d@mail.uk>
2024-02-15 11:35:14 +0100

    feat: create necessary config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
